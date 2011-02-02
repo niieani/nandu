a6ec87a82cd3fbc33d93d44f5e47bc3b1e131ed9
Michał Buczek <michal@buczek.cc>
2011-02-02 22:09:01 +0100

    [fix] fatal in melody manager

--- app/application/modules/default/controllers/MelodyController.php
+++ app/application/modules/default/controllers/MelodyController.php
@@ -3,11 +3,29 @@
 class MelodyController extends Blipoteka_Controller 
 {
 
+	/**
+	 * @var Nandu_Melody_Manager
+	 */
 	protected $_manager;
-	
+
+	public function init()
+	{
+		$this->_manager = new Nandu_Melody_Manager();
+	}
+
 	public function indexAction()
 	{
 		
+	}
+
+	public function evolveAction()
+	{
+		$melodyAId = $this->getRequest()->getParam('a');
+		$melodyBId = $this->getRequest()->getParam('b');
+		$melodyA = $this->_manager->getMelody($melodyAId);
+		$melodyB = $this->_manager->getMelody($melodyBId);
+
+		$this->_helper->jsonOutput(array($melodyAId, $melodyBId));
 	}
 	
 }
